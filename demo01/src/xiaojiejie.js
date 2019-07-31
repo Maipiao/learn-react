25874493baf353679ee2f3f7d236d6c77099234e
Maipiao <18956096465@163.com>
2019-07-31 20:53:47 +0800

    暂存

--- demo01/src/xiaojiejie.js
+++ demo01/src/xiaojiejie.js
@@ -1,6 +1,7 @@
 import React,{Component,Fragment} from 'react'
 
 class Xiaojiejie extends Component {
+  
   // js构造函数 用于其他任何函数执行
   constructor(props){
     super(props)  // 调用父类的构造函数,固定写法
@@ -11,11 +12,12 @@ class Xiaojiejie extends Component {
   }
   render(){
     return (
-      // 最外层必须用标签包裹
-      // 或者在react16中使用Fragment标签
-      // date 2019.7.31
       <Fragment>
-        {/* 在react里写注释 */}
+        {/* 在react里写注释 
+            最外层必须用标签包裹
+            或者在react16中使用Fragment标签
+            date 2019.7.31
+        */}
         <div>
           <input value={this.state.inputValue}  
           onChange={ this.inputChange.bind(this) }
@@ -45,7 +47,6 @@ class Xiaojiejie extends Component {
     绑定事件需要注意的事项:
       一个是this指向不对，你需要重新用bind设置一下指向(ES6的语法)。
       另一个是React中改变值需要使用this.setState方法。
-  
   */
 
   // 输入框事件
