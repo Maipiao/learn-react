513721d1246a871adc4119a83c0dbf2e36072a80
张振亚 <zhangzhenya@zhangzhenyadeMacBook-Pro.local>
2019-10-10 09:19:08 +0800

    list

--- redux/demo01/src/TodoList.js
+++ redux/demo01/src/TodoList.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css'
+import {Input, Button, List} from 'antd'
+
+const data = [
+  '早8点开晨会，分配今天的开发工作',
+  '早9点和项目经理作开发需求讨论会',
+  '晚5:30对今日代码进行review'
+]
 class TodoList extends Component {
   constructor(props) {
     super(props);
@@ -6,8 +14,18 @@ class TodoList extends Component {
   }
   render() { 
     return ( 
-      <div>
-        123
+      <div style={{margin:'10px'}}>
+        <div>
+          <Input placeholder="请输入" style={{width:'250px',marginRight:'10px'}}></Input>
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{width:'325px',marginTop:'10px'}}>
+          <List dataSource={data}
+                bordered
+                renderItem={item=>(<List.Item>{item}</List.Item>)}
+          >
+          </List>
+        </div>
       </div>
     );
   }
